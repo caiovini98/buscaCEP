1cfad565e22e2b03a72790a07f37923c5c5d835e
Caio Vinicius <caiovi98@hotmail.com>
2022-04-24 20:41:45 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
+  const { register, handleSubmit, setValue, setFocus } = useForm();
+
+  const onSubmit = (e: any) => {
+    console.log(e);
+  };
+
+  const checkCEP = async (e: any) => {
+    if (!e.target.value) return;
+    const cep = e.target.value.replace(/\D/g, "");
+    await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setValue("rua", data.logradouro);
+        setValue("bairro", data.bairro);
+        setValue("cidade", data.localidade);
+        setValue("estado", data.uf);
+        setFocus("numero");
+      })
+      .catch((err: any) => console.error(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        CEP: <input type="text" {...register("cep")} onBlur={checkCEP} />
+      </label>
+      <label>
+        Rua: <input type="text" {...register("rua")} />
+      </label>
+      <label>
+        Número: <input type="text" {...register("numero")} />
+      </label>
+      <label>
+        Bairro: <input type="text" {...register("bairro")} />
+      </label>
+      <label>
+        Cidade: <input type="text" {...register("cidade")} />
+      </label>
+      <label>
+        Estado: <input type="text" {...register("estado")} />
+      </label>
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
